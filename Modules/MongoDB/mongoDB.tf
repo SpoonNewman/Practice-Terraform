46a0eae917a04eced23810f8d05bde778825c028
SpoonNewman <rease02@gmail.com>
2024-06-05 20:55:56 -0400

    fixed typos

--- Modules/MongoDB/mongoDB.tf
+++ Modules/MongoDB/mongoDB.tf
@@ -1,11 +1,11 @@
-resource "docker_image" "mondogDB" {
-    name         = "nginx:latest"
+resource "docker_image" "mongo" {
+    name         = "mongo:latest"
     keep_locally = false
 }
 
-resource "docker_container" "mondogDB" {
-    image = docker_image.mondogDB.image_id
-    name = mondogDB
+resource "docker_container" "mongo" {
+    image = docker_image.mongo.image_id
+    name = mongoDB
     ports{
         internal = 80
         external = 8000
