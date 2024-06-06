f53162f9aa79b901bd38046dd46e51ebbb90b03e
SpoonNewman <rease02@gmail.com>
2024-06-05 21:01:32 -0400

    tabbed ports for vault so its more readable

--- Modules/Vault/HC_vault.tf
+++ Modules/Vault/HC_vault.tf
@@ -6,6 +6,7 @@ resource "docker_image" "vault" {
 resource "docker_container" "vault" {
     image = docker_image.vault.image_id
     name = vault
+    
     ports{
         internal = 80
         external = 8000
