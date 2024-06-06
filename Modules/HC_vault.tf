5b58a0cb0d735fbcbabc76565e7dc1b3bdb15bb1
SpoonNewman <rease02@gmail.com>
2024-06-05 20:57:56 -0400

    added resources for vault

---
+++ Modules/HC_vault.tf
@@ -0,0 +1,13 @@
+resource "docker_image" "vault" {
+    name         = "vault:latest"
+    keep_locally = false
+}
+
+resource "docker_container" "vault" {
+    image = docker_image.vault.image_id
+    name = vault
+    ports{
+        internal = 80
+        external = 8000
+    }
+}
